aa78ac99201027fd15df8c08ae096d461bbe173a
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-06-17 17:19:41 +0200

    Improved the mobile menu (unhandled bug occured when clicking the last menu element)

--- themes/nova.warsawzoli-theme/assets/js/c-nova.menu.js
+++ themes/nova.warsawzoli-theme/assets/js/c-nova.menu.js
@@ -1,34 +1,63 @@
-document.addEventListener('DOMContentLoaded', function () {
-    mobileMenuClick();
+document.addEventListener("DOMContentLoaded", function() {
+	mobileMenuClick();
 });
 
 function mobileMenuClick() {
-    document.querySelectorAll('#mobileMenu.links>li:not(:first-child)').forEach((link) => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            // let correspondingSubmenu = link.nextElementSibling;
-            let previousActiveLink = document.querySelector('#mobileMenu.links>li.active');
-            if (previousActiveLink === null) {
-                link.classList.add('active');
-                expandOrHideSubmenu(link, "expand");
-            } else if (previousActiveLink === link) {
-                previousActiveLink.classList.remove('active');
-                expandOrHideSubmenu(previousActiveLink, "hide");
-            } else {
-                previousActiveLink.classList.remove('active');
-                expandOrHideSubmenu(previousActiveLink, "hide");
-                link.classList.add('active');
-                expandOrHideSubmenu(link, "expand");
-
-            }
-        })
-    })
+	document
+		.querySelectorAll("#mobileMenu.links>li:not(:first-child)")
+		.forEach((link, li_index) => {
+			let menuLink = link.firstElementChild.getAttribute("data-menu-name");
+			let matchingSubmenu = document.querySelector(
+				`#desktop-submenu[data-menu-name="${menuLink}"]`
+			);
+			link.addEventListener("click", event => {
+				if (matchingSubmenu) {
+					event.preventDefault();
+				} else {
+					return;
+				}
+				let previousActiveLink = document.querySelector(
+					"#mobileMenu.links>li.active"
+				);
+				if (previousActiveLink === null) {
+					link.classList.add("active");
+					expandOrHideSubmenu(link, "expand");
+					if (
+						document.querySelectorAll("#mobileMenu.links>li").length >= 9 &&
+						li_index >= 5
+					) {
+						document
+							.querySelector("#mobileMenu")
+							.classList.add("height-adjusted");
+					}
+				} else if (previousActiveLink === link) {
+					previousActiveLink.classList.remove("active");
+					expandOrHideSubmenu(previousActiveLink, "hide");
+					if (
+						document.querySelectorAll("#mobileMenu.links>li").length >= 9 &&
+						li_index >= 5
+					) {
+						document
+							.querySelector("#mobileMenu")
+							.classList.remove("height-adjusted");
+					}
+				} else {
+					previousActiveLink.classList.remove("active");
+					expandOrHideSubmenu(previousActiveLink, "hide");
+					link.classList.add("active");
+					expandOrHideSubmenu(link, "expand");
+				}
+			});
+		});
 }
 
 function expandOrHideSubmenu(linkNode, expand) {
-    if (expand === "expand") {
-        return linkNode.nextElementSibling.classList.add('active');
-    } else {
-        return linkNode.nextElementSibling.classList.remove('active');
-    }
+	if (linkNode.nextElementSibling === null) {
+		return;
+	}
+	if (expand === "expand") {
+		return linkNode.nextElementSibling.classList.add("active");
+	} else {
+		return linkNode.nextElementSibling.classList.remove("active");
+	}
 }
